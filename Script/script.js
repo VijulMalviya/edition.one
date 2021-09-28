7e6cb54dac4b450bfbb1eb8e8a5ece4b3a75876b
Vijul Malviya <50556784+VijulMalviya@users.noreply.github.com>
2021-09-28 18:21:15 +0530

    Add files via upload

---
+++ Script/script.js
@@ -0,0 +1,69 @@
+
+$(document).ready(function(){
+    $("#bars").click(function(){
+    
+      if( $("#bars i").hasClass("ri-bar-chart-horizontal-line")){
+      $(".navlinks").fadeIn();
+      $("#bars i").removeClass("ri-bar-chart-horizontal-line");
+      $("#bars i").addClass("ri-close-line");
+     
+      }else{
+        $(".navlinks").fadeOut();
+      $("#bars i").addClass("ri-bar-chart-horizontal-line");
+      $("#bars i").removeClass("ri-close-line");
+      return true;
+      }
+    });
+  });
+
+  // When the user scrolls down 50px from the top of the document, resize the header's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+    document.querySelector(".nav").style.backgroundColor = "black";
+  } else {
+    document.querySelector(".nav").style.backgroundColor = "transparent";
+    
+  }
+}
+
+
+
+$('.main-text h1').textillate({
+    in: {
+        effect: 'fadeInUp'
+    },
+    loop: true,
+    out: {
+        effect: 'fadeOutRight'
+    }
+})
+
+gsap.to('#smalltolarge',{
+    scrollTrigger:{
+        trigger: "#btn",
+        start : "top top",
+        scrub: 2,
+        pin: "#fullimg"
+    },
+    width:"80%",
+    greyscale : "1"
+})
+
+
+var mq = window.matchMedia( "(max-width: 992px)" );
+if (mq.matches) {
+
+    gsap.to('#smalltolarge',{
+        scrollTrigger:{
+            trigger: "#btn",
+            start : "top top",
+            scrub: 2,
+            pin: "#fullimg"
+        },
+        width:"100%",
+        greyscale : "1"
+    })
+ }
+
